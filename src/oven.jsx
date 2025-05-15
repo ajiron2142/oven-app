b2ef6d12f180608b4da52748efc2f9523f1f7255
Andres <aijiron2142@gmail.com>
2025-05-15 16:39:39 -0600

    Successfully pulling from FastAPI

--- src/oven.jsx
+++ src/oven.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react'
+
 export default function Oven()
 {
+    const fastAPIUrl = 'http://10.0.0.178:8000'
+    const cameraInfoEndpoint = '/camera-info'
+    
+    const [oven, setOven ] = useState([])
+
+    const getCameraInfo = async () =>
+    {
+        const response = await fetch (`${fastAPIUrl}${cameraInfoEndpoint}`)
+        const result = await response.json()
+        
+        setOven(result)
+        // console.log(result)
+        // console.log(map.get('model'))
+    }
+
+    useEffect(() =>
+    {
+        getCameraInfo()    
+    }, [])
+    
+
     console.log("Hi from the console")
 
-    return <div>
+    return <>
+        { oven.map((value, index) =>
+            <button key={index}> { value.Model } </button>) }
+        
         <button>
             Click Me
-        </button>
-    </div>
+        </button>    
+    </>
 }
