795a5168998402cf46f03b7298815f86e7fe50f8
Andres <aijiron2142@gmail.com>
2025-05-15 22:04:15 -0600

    Imported Metadata

--- src/oven.jsx
+++ src/oven.jsx
@@ -4,29 +4,36 @@ export default function Oven()
 {
     const fastAPIUrl = 'http://10.0.0.178:8000'
     const cameraInfoEndpoint = '/camera-info'
-    
-    const [oven, setOven ] = useState([])
+    const metadataEndpoint = '/metadata'
+
+    const [camera, setCamera ] = useState([])
+    const [metadata, setMetadata] = useState([])
 
     const getCameraInfo = async () =>
     {
         const response = await fetch (`${fastAPIUrl}${cameraInfoEndpoint}`)
         const result = await response.json()
         
-        setOven(result)
-        // console.log(result)
-        // console.log(map.get('model'))
+        setCamera(result)
+    }
+    
+    const getMetadata = async () =>
+    {
+        const response = await fetch (`${fastAPIUrl}${metadataEndpoint}`)
+        const result = await response.json()
+
+        setMetadata(result)
+        console.log(result)
     }
 
     useEffect(() =>
     {
-        getCameraInfo()    
+        getCameraInfo();
+        getMetadata() 
     }, [])
-    
-
-    console.log("Hi from the console")
 
     return <>
-        { oven.map((value, index) =>
+        { camera.map((value, index) =>
             <button key={index}> { value.Model } </button>) }
         
         <button>
